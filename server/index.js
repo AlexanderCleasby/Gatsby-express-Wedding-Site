ad9a1dbf622d4624a52761ffa8be8eeb5efe6a82
alexandercleasby <cleasby.alex@gmail.com>
2019-11-14 16:13:04 -0500

    added a server script to run the express server in dev mode.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const gatsbyExpress = require('gatsby-plugin-express');
+const mongoose = require('mongoose')
 const app = express()
+
+if (process.env.NODE_ENV === 'development'){
+  require('dotenv').config()
+}
+
+
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true, useUnifiedTopology: true},()=>console.log('connected to db'))
 
 app.get('/api',(req,res)=>{
     console.log('foo')
