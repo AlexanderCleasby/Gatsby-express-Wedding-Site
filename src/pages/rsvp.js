9e24df427616b48b0e289e0fc42c66bf5d20d288
alexandercleasby <cleasby.alex@gmail.com>
2019-11-16 13:04:11 -0500

    pass events as props to RSVP component

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -1,16 +1,30 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import RSVP from "../components/rsvp/index";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const RSVPage = () => (
-  <Layout>
-    <SEO title="RSVP" />
-    <h1>RSVP Here</h1>
-    <RSVP />
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+
+
+const RSVPage = () => {
+  const eventsData = useStaticQuery(graphql`
+    {
+        allEventsJson {
+            nodes {
+                title
+                desc
+                address
+                coords { lat lng }
+            }
+        }
+    }`).allEventsJson.nodes
+  return(
+    <Layout>
+      <SEO title="RSVP" />
+      <h1>RSVP Here</h1>
+      <RSVP events={eventsData} />
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+)}
 
 export default RSVPage
