df51c705dd7d36aac3c28137df68b5793c2f8011
alexandercleasby <cleasby.alex@gmail.com>
2019-11-16 11:40:48 -0500

    set up rsvp component to consume the invite api.

--- src/components/rsvp/index.js
+++ src/components/rsvp/index.js
@@ -15,9 +15,23 @@ class Rsvp extends React.Component{
     submitCode=(e)=>{
         e.preventDefault()
         fetch('/api/invite',{method: 'POST',body:JSON.stringify({code:this.state.code}),headers: {"Content-Type": "application/json"}})
+        .then((res)=>{
+            if (!res.ok){
+                throw Error(res.status)
+            }
+            else{
+                return res.json()
+            }
+        })
+        .then((res)=>this.setState((prev)=>({...prev,name:res.name,plus1s:res.plus1s})))
+        .catch((err)=>{console.log(err.message)})
     }
 
-    valChange = (e)=>this.setState({[e.target.name]:e.target.value.toUpperCase()})
+    valChange = (e)=>{
+        if(e.target.value.length<=5){
+            this.setState({[e.target.name]:e.target.value.toUpperCase()})
+        }
+    }
 
     render(){
         return <CodeForm submit = {this.submitCode} valChange = {this.valChange} code={this.state.code} />
