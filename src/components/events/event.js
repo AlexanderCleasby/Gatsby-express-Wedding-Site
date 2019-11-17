4965adfb9a95e0952d07ac4ba72221b7d88213ad
alexandercleasby <cleasby.alex@gmail.com>
2019-11-17 09:29:32 -0500

    updated event to include google placeID for navigation

--- src/components/events/event.js
+++ src/components/events/event.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import Map from './map'
+import './events.scss'
 
-
-const Event = ({title,desc,children,coords,showMap})=>{
-    return <div>
-        <h2>
-            {title}
-        </h2>
+const Event = ({title,desc,address,children,coords,placeId,venue,showMap})=>{
+    return <div className="event">
+        <div className="header">
+            <h2>
+                {title}
+            </h2>
+            <h4>
+                {address}
+                <a href={`https://www.google.com/maps/search/?api=1&query=${venue}&query_place_id=${placeId}`}>Nav</a>
+            </h4>
+        </div>
         <p>
             {desc}
         </p>
-        {(()=>showMap ? <Map coords={coords} /> : "")()}
         {children}
+        {(()=>showMap ? <Map coords={coords} /> : "")()}
     </div>
 }
 
