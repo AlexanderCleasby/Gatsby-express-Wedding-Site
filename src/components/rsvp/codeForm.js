c5698c2393d6a811fce6d49713d3307aee151b2e
alexandercleasby <cleasby.alex@gmail.com>
2019-11-17 14:41:16 -0500

    added style to codeForm

--- src/components/rsvp/codeForm.js
+++ src/components/rsvp/codeForm.js
@@ -4,9 +4,9 @@ import React from "react";
 
 const codeForm = (props)=>(
     <form onSubmit={props.submit}>
-        <input name="code" value={props.code} onChange={props.valChange} />
+        <input name="code" value={props.code} onChange={props.valChange} className=" col-sm-2 form-control" />
         <div style={{color:props.code.length > 5 ? "red" : "black" }}>{props.code.length}/5</div>
-        <input type="submit" />
+        <input className="btn btn-primary" type="submit" />
     </form>
 )
 
