cde989893ff6c5697a90f88e7594c147e79b7871
alexandercleasby <cleasby.alex@gmail.com>
2019-11-25 12:21:24 -0500

    removed link to page 2 on homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import Events from "../components/events"
 
@@ -10,7 +9,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Events />
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
